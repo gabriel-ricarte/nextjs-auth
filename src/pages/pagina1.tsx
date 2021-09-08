d1b901bd48614a26387ecb028f5762471a695162
Gabriel Ricarte <gabriel.ricarte@itarget.com.br>
2021-09-08 15:56:47 -0300

    criando paginas estáticas e dinamicas com react

---
+++ src/pages/pagina1.tsx
@@ -0,0 +1,19 @@
+import { GetServerSideProps } from "next";
+
+const Pagina1Page = ({name}: {name:string}) => {
+    return (
+        <div>
+            Pagina1 - {name}
+        </div>
+    );
+};
+
+export default Pagina1Page;
+
+export const getServerSideProps: GetServerSideProps =  async(ctx) => {
+    return {
+        props: {
+            name: 'Gabriel Ricarte'
+        }
+    }
+}
